4d8122f85462b87189b75a6c29d5704f8583b6a1
Nabil BOUTEMEUR <nbouteme@e3r12p17.42.fr>
2016-08-02 23:32:42 +0200

    Meilleur cam

--- src/cpu_driver.c
+++ src/cpu_driver.c
@@ -6,7 +6,7 @@
 /*   By: nbouteme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/22 00:33:55 by nbouteme          #+#    #+#             */
-/*   Updated: 2016/08/01 23:53:05 by nbouteme         ###   ########.fr       */
+/*   Updated: 2016/08/02 23:31:09 by nbouteme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ t_ray gen_camray(int x, int y, t_vec3 pos, t_vec3 dir)
 	t_vec3 u, v, tmp1, tmp2, tmp3;
 	t_vec3 viewPlaneBottomLeftPoint, lap;
 
-	float viewPlaneHalfWidth = tan(M_PI / 4);
+	float viewPlaneHalfWidth = tan(M_PI / 8);
 	float aspectRatio = 720.0f / 1280.0f;
 	float viewPlaneHalfHeight = aspectRatio * viewPlaneHalfWidth;
 
@@ -63,27 +63,36 @@ t_ray gen_camray(int x, int y, t_vec3 pos, t_vec3 dir)
 	vec3_normalize(u);
 	vec3_normalize(v);
 
-	ft_memcpy(v, vec3_muls(v, viewPlaneHalfHeight), sizeof(t_vec3));
-	ft_memcpy(u, vec3_muls(u, viewPlaneHalfWidth), sizeof(t_vec3));
+	ft_memcpy(tmp1, vec3_muls(v, viewPlaneHalfHeight), sizeof(t_vec3));
+	ft_memcpy(tmp2, vec3_muls(u, viewPlaneHalfWidth), sizeof(t_vec3));
 
-	ft_memcpy(tmp3, vec3_sub(v, u), sizeof(t_vec3));
+	ft_memcpy(tmp3, vec3_sub(tmp1, tmp2), sizeof(t_vec3));
 	ft_memcpy(viewPlaneBottomLeftPoint, vec3_sub(lap, tmp3), sizeof(t_vec3));
 
-	ft_memcpy(v, vec3_muls(v, 2.0f), sizeof(t_vec3));
-	ft_memcpy(u, vec3_muls(u, 2.0f), sizeof(t_vec3));
+	// tmp1 = v * viewPlaneHalfHeight;
+	// tmp2 = u * viewPlaneHalfWidth;
+	// tmp3 = tmp1 - tmp2;
+	// viewPlaneBottomLeftPoint = lap - tmp3;
 
-	ft_memcpy(v, vec3_muls(v, 1.0f / 720.0f), sizeof(t_vec3));
-	ft_memcpy(u, vec3_muls(u, 1.0f / 1280.0f), sizeof(t_vec3));
+	ft_memcpy(v, vec3_muls(v, viewPlaneHalfHeight * 2.0f / 720.0f), sizeof(t_vec3));
+	ft_memcpy(u, vec3_muls(u, viewPlaneHalfWidth * 2.0f / 1280.0f), sizeof(t_vec3));
 
-	// ^ Suppose etre fait une seul fois par camera
+	// v *= viewPlaneHalfHeight * 2.0f / 720.0f;
+	// u *= viewPlaneHalfWidth * 2.0f / 1280.0f;
+// ^ Suppose etre fait une seul fois par camera
 
 	t_vec3 ppoint;
 
-	ft_memcpy(tmp1, vec3_muls(u, x), sizeof(t_vec3));
-	ft_memcpy(tmp2, vec3_muls(v, y), sizeof(t_vec3));
-	ft_memcpy(tmp2, vec3_add(tmp2, tmp1), sizeof(t_vec3));
 
-	ft_memcpy(ppoint, vec3_add(viewPlaneBottomLeftPoint, tmp2), sizeof(t_vec3));
+	ft_memcpy(tmp1, vec3_muls(u, -x), sizeof(t_vec3));
+	ft_memcpy(tmp2, vec3_muls(v, y), sizeof(t_vec3));
+	ft_memcpy(tmp3, vec3_add(tmp2, tmp1), sizeof(t_vec3));
+
+	// tmp1 = u * x;
+	// tmp2 = v * y;
+	// tmp3 = tmp2 + tmp1;
+	// ppoint = viewPlaneBottomLeftPoint + tmp3;
+	ft_memcpy(ppoint, vec3_add(viewPlaneBottomLeftPoint, tmp3), sizeof(t_vec3));
 
 	return (gen_ray(pos, ppoint));
 }
@@ -92,9 +101,7 @@ t_ray gen_ray(t_vec3 from, t_vec3 to)
 {
 	t_ray ret;
 
-	ret.dir[0] = to[0] - from[0];
-	ret.dir[1] = to[1] - from[1];
-	ret.dir[2] = to[2] - from[2];
+	ft_memcpy(ret.dir, vec3_sub(to, from), sizeof(t_vec3));
 	vec3_normalize(ret.dir);
 	ft_memcpy(ret.pos, from, sizeof(t_vec3));
 	return (ret);
