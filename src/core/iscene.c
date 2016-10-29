fe6ab59c3fd31a0080925cccb7199f78c7d995ad
Nabil BOUTEMEUR <nbouteme@e1r10p4.42.fr>
2016-10-30 01:25:30 +0200

    Fix uninit value

--- src/core/iscene.c
+++ src/core/iscene.c
@@ -6,7 +6,7 @@
 /*   By: nbouteme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/01 11:55:04 by nbouteme          #+#    #+#             */
-/*   Updated: 2016/10/28 01:23:49 by nbouteme         ###   ########.fr       */
+/*   Updated: 2016/10/30 01:23:06 by nbouteme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_iscene	*load_iscene(const char *fn)
 	buf[1] = from_file("lang", &size, scene_file_check);
 	l = lang_parser(buf[1]);
 	p = l ? get_parser(l, "file") : 0;
-	buf[3] = 0;
+	ft_memset(buf + 2, 0, sizeof(void*) * 2);
 	if (l && buf[0] && run_parser(p, buf[0], &buf[2]))
 	{
 		buf[3] = gen_iscene(buf[2], buf[0]);
