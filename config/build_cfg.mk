a4107d06e97e6b52591708290c006e406f9011c2
Nabil BOUTEMEUR <nbouteme@e1r9p3.42.fr>
2016-11-08 15:40:20 +0100

    Remove GPU module. Fuck you Apple/nvidia

--- config/build_cfg.mk
+++ config/build_cfg.mk
@@ -1,7 +1,7 @@
 NAME = rtv1
 TYPE = prog
 
-MODULES = src
+MODULES = src mcpu
 #CFLAGS = -Wall -Wextra -Werror -g
 CFLAGS = -Wall -Wextra -Werror -flto -Ofast -march=native -mtune=native -ffinite-math-only -funsafe-math-optimizations -fno-math-errno  -ffast-math
 LFLAGS = $(CFLAGS) -lm
@@ -12,19 +12,4 @@ OUTPUT = $(NAME)
 
 ifneq ($(filter mcpu,$(MODULES)),)
 LFLAGS += -lpthread
-endif
-
-ifneq ($(filter gpu,$(MODULES)),)
-
-ifeq (($shell uname),Darwin)
-CUDA_LIBS = /opt/cuda/lib64
-else
-CUDA_LIBS = /Developer/NVIDIA/CUDA-7.5/lib
-LFLAGS += -Wl,-rpath,$(CUDA_LIBS)
-endif
-
-gpu_CC := nvcc
-gpu_CFLAGS = -g --std=c++11 -ccbin g++ -m64 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_30,code=sm_30 $(addprefix -I,$(INCLUDE_DIRS_ACC))
-gpu_EXT := .cu
-LFLAGS += -L$(CUDA_LIBS) -lcudart -lstdc++
 endif
